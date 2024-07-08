b52a04c2b9a8519fcaf55051c08e452a87266fdf
Grisha Angelov <grisha.angelov89@gmail.com>
2024-07-08 10:34:27 +0300

    relocate all area charts to separate directory

--- src/components/ui/charts/index.js
+++ src/components/ui/charts/index.js
@@ -1,13 +1,13 @@
-import AreaChart from "./area-chart"
-import LinearAreaChart from "./linear-area-chart"
-import StepAreaChart from "./step-area-chart"
-import StackedAreaChart from "./stacked-area-chart"
-import StackedExpandedAreaChart from "./stacked-expanded-area-chart"
-import LegendAreaChart from "./legend-area-chart"
-import IconsAreaChart from "./icons-area-chart"
-import GradientAreaChart from "./gradient-area-chart"
-import AxesAreaChart from "./axes-area-chart"
-import InteractiveAreaChart from "./interactive-area-chart"
+import AreaChart from "./area-charts/area-chart"
+import LinearAreaChart from "./area-charts/linear-area-chart"
+import StepAreaChart from "./area-charts/step-area-chart"
+import StackedAreaChart from "./area-charts/stacked-area-chart"
+import StackedExpandedAreaChart from "./area-charts/stacked-expanded-area-chart"
+import LegendAreaChart from "./area-charts/legend-area-chart"
+import IconsAreaChart from "./area-charts/icons-area-chart"
+import GradientAreaChart from "./area-charts/gradient-area-chart"
+import AxesAreaChart from "./area-charts/axes-area-chart"
+import InteractiveAreaChart from "./area-charts/interactive-area-chart"
 
 const charts = [
   AreaChart,
